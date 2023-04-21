c0344188dac0a61316e34c91f7bb5f1f2ccc7515
deolafemi <adeolaadewusi12@yahoo.com>
2023-04-21 16:36:36 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
+
 /**
-*main - A program that print a line using the print function
-*Return: 0
-*
+
+* main -5 -printf.c
+
+* Return: 0
+
 */
+
 int main(void)
+
 {
+
 printf("with proper grammar, but the outcome is a piece of art,\n");
+
 return (0);
+
 }  	
