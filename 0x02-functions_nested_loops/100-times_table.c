93e15c7b894383e6ea3e93e9bb61105c057cde82
deolafemi <adeolaadewusi12@yahoo.com>
2023-04-25 13:31:23 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -25,7 +25,7 @@ _putchar(' ');
 _putchar(' ');
 _putchar(' ');
 _putchar(k + '0');
-} 
+{ 
 else if (k >= 10 && k < 100)
 {
 _putchar(',');
@@ -33,7 +33,7 @@ _putchar(' ');
 _putchar(' ');
 _putchar((k / 10) + '0');
 _putchar((k % 10) + '0');
-} 
+{ 
 else if (k >= 100)
 {
 _putchar(',');
