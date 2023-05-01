54e23b0f63081f4dd020048f3301fb3105565427
deolafemi <adeolaadewusi12@yahoo.com>
2023-04-30 23:05:26 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,14 +9,11 @@ void print_line(int n)
 if (n <= 0)
 {
 _putchar('\n');
-}else
-{
-int i;
+return;
+}
 
-for (i = 1; i <= n; i++)
-{
-_putchar('_');
-}
+int i;
+for(i = 0; i < n; i++)
 _putchar('\n');
-}
+_putchar('_');
 }
